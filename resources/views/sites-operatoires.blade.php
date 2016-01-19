1c3276234335cc4db2ec222042d3a67c299cfd8d
Antoine Augusti <antoine.augusti@gmail.com>
2016-01-19 12:42:31 +0100

    Ajout Saint-Hilaire

--- resources/views/sites-operatoires.blade.php
+++ resources/views/sites-operatoires.blade.php
@@ -63,4 +63,37 @@
       </div>
     </div>
 </div>
+
+<hr>
+
+{{-- Saint-Hilaire --}}
+<div class="row">
+    <div class="col-md-6">
+      <h2><span class="badge">Rouen</span> Clinique Saint-Hilaire</h2>
+        La <a href="http://www.clinique-sainthilaire.fr" title="Clinique Saint-Hilaire">clinique Saint-Hilaire</a> est située, à proximité du CHU de Rouen, à l'adresse suivante :<br>
+      <div class="adresse-physique">
+        Clinique Saint-Hilaire<br>
+        2 place Saint-Hilaire<br>
+        76000 Rouen<br>
+      </div>
+
+      <h3>Accès</h3>
+      <p>
+        Un parking gratuit pendant 1h est disponible devant la clinique. Des places handicapées sont à votre disposition si vous êtes en situation de handicap. Un système de vidéosurveillance est en place.
+      </p>
+      <p>
+        La clinique est accessible via les transports en commun du <a href="http://reseau-astuce.fr" title="Réseau Astuce">réseau Astuce</a> de l'agglomération de Rouen :
+        <ul>
+          <li>Métro : station terminus <em>Boulingrin</em> (à 500 m)</li>
+          <li>Métrobus TEOR lignes 1, 2 et 3 : station <em>Saint-Hilaire</em> (en face de la clinique)</li>
+        </ul>
+      </p>
+    </div>
+    <div class="col-md-6">
+      <div class="google-maps margin-top-50">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d10377.047141200135!2d1.114!3d49.441769!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xe7c928bdde19b64!2sClinique+Saint-Hilaire!5e0!3m2!1sen!2sus!4v1453203671979" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+      </div>
+    </div>
+</div>
+
 @stop
