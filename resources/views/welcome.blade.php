084c68aea9760097e5913c4197b9b191c8d23e05
Antoine Augusti <antoine@augusti.fr>
2016-02-01 22:12:17 +0100

    Changement nom hospitalisation

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,7 +30,7 @@
             <a class="btn btn-default" href="{{ route('equipe') }}">Voir toute l'équipe et le bloc</a>
         </div>
         <div class="col-md-4">
-            <h2>Heading 3</h2>
+            <h2>L'hospitalisation</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe rem nisi accusamus error velit animi non ipsa placeat. Recusandae, suscipit, soluta quibusdam accusamus a veniam quaerat eveniet eligendi dolor consectetur.</p>
             <a class="btn btn-default" href="#">More Info</a>
         </div>
