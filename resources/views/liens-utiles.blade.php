a25a5a77fdcf8301ac384b3226ff1d3d381cb7e5
Antoine Augusti <antoine.augusti@gmail.com>
2016-02-11 22:18:01 +0100

    Ordre des liens

--- resources/views/liens-utiles.blade.php
+++ resources/views/liens-utiles.blade.php
@@ -4,7 +4,7 @@
     <h2 class="margin-bottom-30">Liens utiles</h2>
     <ul>
         <li><a href="http://urofrance.org/accueil.html">Site de l'association française d'urologie</a>. Vous y trouverez beaucoup de références.</li>
-        <li><a href="http://www.ameli.fr">Le site de l'assurance maladie</a>.</li>
         <li><a href="http://www.e-cancer.fr">Le site de l'Institut National Du Cancer</a>.</li>
+        <li><a href="http://www.ameli.fr">Le site de l'assurance maladie</a>.</li>
     </ul>
 @stop
