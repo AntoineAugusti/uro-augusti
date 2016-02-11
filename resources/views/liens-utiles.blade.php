7d56d304f27660ec5bfe43458cf497a82d610a5f
Antoine Augusti <antoine.augusti@gmail.com>
2016-02-11 22:31:01 +0100

    Clinique Megival

--- resources/views/liens-utiles.blade.php
+++ resources/views/liens-utiles.blade.php
@@ -5,6 +5,7 @@
     <ul>
         <li><a href="http://urofrance.org/accueil.html">Site de l'association française d'urologie</a>. Vous y trouverez beaucoup de références.</li>
         <li><a href="http://www.e-cancer.fr">Le site de l'Institut National Du Cancer</a>.</li>
+        <li><a href="http://www.megival.com">Clinique Mégival</a>.</li>
         <li><a href="http://www.ameli.fr">Le site de l'assurance maladie</a>.</li>
     </ul>
 @stop
