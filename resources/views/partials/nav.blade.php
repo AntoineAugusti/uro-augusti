da3ab95b721c44049f65871a921da678f87bed2f
Antoine Augusti <antoine.augusti@gmail.com>
2016-02-12 15:42:16 +0100

    Changement titre du site

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -7,7 +7,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/" title="Accueil">Docteur Michel Augusti</a>
+      <a class="navbar-brand" href="/" title="Accueil">Dr Michel Augusti</a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
