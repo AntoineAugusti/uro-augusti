1560e1cbdc5fb54ba259b00aa76abde2a72dc0e9
Antoine Augusti <antoine.augusti@gmail.com>
2016-02-13 10:05:41 +0100

    404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+@extends('partials.master')
+
+@section('content')
+    <h2 class="margin-bottom-30">Il n'y a rien ici</h2>
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <img class="img-responsive img-rounded" src="assets/images/404.gif" alt="404">
+        </div>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    Vous avez tout cassé. C'est une erreur 404, voilà. Urosement, vous pouvez <a href="/">retourner à l'accueil</a>.
+@stop
