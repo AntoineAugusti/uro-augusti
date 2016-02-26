14f0e7a91489e088dbd479d7014729711acf32ce
Antoine Augusti <antoine.augusti@gmail.com>
2016-02-26 11:02:39 +0100

    More link tags in header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -4,14 +4,20 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title }} - Dr Michel Augusti</title>
     <meta name="description" content="{!! $description !!}">
+
     <meta name="author" content="Michel Augusti"/>
 
+    <!-- Google verification -->
     <meta name="google-site-verification" content="yIrmHc13V2t9G-Boeq4ES83JmLnUFGugld0I8KjIFRA" />
-    <link rel="publisher" href="https://plus.google.com/113939250764481617219" />
+    <link href="https://plus.google.com/113939250764481617219" rel="publisher" />
 
-    <title>{{ $title }} - Dr Michel Augusti</title>
+    <!-- DNS prefetch -->
+    <link href='//fonts.googleapis.com' rel="dns-prefetch">
+    <link href='//fonts.gstatic.com' rel="dns-prefetch">
 
+    <!-- Assets -->
     <link rel="stylesheet" href="{{ elixir('assets/css/all.css') }}">
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
