a26d96372fb0b9966dfb29f1908fd1125349fa6a
Antoine Augusti <antoine.augusti@gmail.com>
2016-04-02 14:25:36 +0200

    Ajout photos equipe

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -3,11 +3,30 @@
 @section('content')
     <h2 class="margin-bottom-30">L'équipe</h2>
     <div class="row">
-        <div class="col-md-4 col-vertical-center">
+        <div class="col-md-6 col-vertical-center">
             <img class="img-responsive img-rounded" src="assets/images/bloc.jpg" alt="Bloc opératoire">
         </div>
-        <div class="col-md-8 col-vertical-center">
+        <div class="col-md-6 col-vertical-center">
             Le Dr Michel Augusti au bloc opératoire de la clinique Saint-Hilaire à Rouen, traitant par <a href="http://urofrance.org/nc/lurologie-grandpublic/fiches-patient/resultats-de-la-recherche/html/vaporisation-dadenome-prostatique-par-laser-greenlight.html">photovaporisation laser</a>, une l'hypertrophie bénigne de la prostate.
         </div>
+
+        <hr style="margin: 50px 0">
+
+        <div class="col-md-6 col-vertical-center">
+            L'équipe du bloc de Mégival. De gauche à droite : Pascaline, Mylène, Villeme, Florian, Karine, Nolwenn.
+        </div>
+        <div class="col-md-6 col-vertical-center">
+            <img class="img-responsive img-rounded" src="assets/images/equipe-bloc.jpg" alt="Équipe du bloc opératoire">
+        </div>
+
+        <hr style="margin: 50px 0">
+
+        <div class="col-md-6 col-vertical-center">
+            <img class="img-responsive img-rounded" src="assets/images/roselyne.jpg" alt="Roselyne Stallin">
+        </div>
+        <div class="col-md-6 col-vertical-center">
+            Roselyne Stallin, la secrétaire médicale dédiée à l'urologie.
+        </div>
+
     </div>
 @stop
