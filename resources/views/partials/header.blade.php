c2ddd174963e02ea5df1f778058e5bce57ed5bf8
Antoine Augusti <antoine.augusti@gmail.com>
2016-11-19 19:53:41 +0100

    Make pages compatible with AMP ⚡️

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,11 +1,14 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html ⚡ lang="fr">
 <head>
     <meta charset="utf-8">
+    <script async src="https://cdn.ampproject.org/v0.js"></script>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1">
     <title>{{ $title }} - Dr Michel Augusti</title>
     <meta name="description" content="{!! $description !!}">
+
+    <link rel="canonical" href="{{ Request::fullUrl() }}" />
 
     <meta name="author" content="Michel Augusti"/>
 
@@ -25,6 +28,9 @@
     <![endif]-->
     <link rel="icon" type="image/x-icon" href="favicon.ico" />
     <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
+
+    <!-- AMP -->
+    @include('partials.amp')
 
     <script>
       (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
