84ee5929a2f35c92df5b49febe7ee50b4ff79028
Antoine Augusti <antoine.augusti@gmail.com>
2020-07-21 17:43:33 -0400

    Clear button

--- resources/views/partials/banner.blade.php
+++ resources/views/partials/banner.blade.php
@@ -1,6 +1,9 @@
 <div class="alert alert-danger">
   ⚠️ COVID-19 : avant une consultation ou une opération, vous devez remplir un formulaire.
-  <a href="/covid-19">
-    <button class="btn btn-default btn-sm pull-right">S'informer</button>
-  </a>
+  <div class="pull-right">
+    <a href="/covid-19">
+      <button class="btn btn-default btn-sm">S'informer</button>
+    </a>
+  </div>
+  <div class="clearfix"></div>
 </div>
