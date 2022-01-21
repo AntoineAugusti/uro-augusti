827b1827437d2953391f44558d70458c8935aff7
Antoine Augusti <antoine.augusti@gmail.com>
2022-01-21 14:40:06 +0100

    Remove banner

--- resources/views/partials/banner.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="alert alert-danger">
-  ⚠️ COVID-19 : avant une consultation ou une opération, vous devez remplir un formulaire. Il est inutile de l'imprimer.
-  <div class="pull-right">
-    <a href="/covid-19">
-      <button class="btn btn-default btn-sm">S'informer</button>
-    </a>
-  </div>
-  <div class="clearfix"></div>
-</div>
